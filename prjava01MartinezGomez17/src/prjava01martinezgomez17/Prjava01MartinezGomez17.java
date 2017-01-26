62d85f962ad9b2687b957760bd52538b9efb96b9
CarlosMartinez <CarlosMartinez@10.0.2.15>
2017-01-26 17:28:47 +0100

    Versió 0.2 del projecte prjava01MartinezGomez17

---
+++ prjava01MartinezGomez17/src/prjava01martinezgomez17/Prjava01MartinezGomez17.java
@@ -0,0 +1,25 @@
+package prjava01martinezgomez17;
+
+import java.io.*;
+public class Prjava01MartinezGomez17 {
+
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer01MartinezGomez17.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
+            bw.write("<html>"); bw.newLine();
+            bw.write("    <head>"); bw.newLine();
+            bw.write("        <title>"); bw.newLine();
+            bw.write("            Nova p&agrave;gina web de MartinezGomez17"); bw.newLine();
+            bw.write("        <title>"); bw.newLine();
+            bw.write("    </head>"); bw.newLine();
+            bw.write("    <body>"); bw.newLine();
+            bw.write("        <p>Nova p&agrave;gina web de MartinezGomez17</p>"); bw.newLine();
+            bw.write("        <p>DAW2 UF4 Pr&agrave;ctica 2</p>"); bw.newLine();
+            bw.write("    </body>"); bw.newLine();
+            bw.write("</html>"); bw.newLine();
+            bw.close();
+        }
+    }
+    
+}
+
